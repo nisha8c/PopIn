77cf679c7ba48fb714ad91e83ba015101cb998f4
salt-dev <salt-dev@salt-devs-MacBook-Pro.local>
2022-11-25 13:14:30 +0100

    Adds TimeSheet list

--- pages/TimeSheet.js
+++ pages/TimeSheet.js
@@ -1,16 +1,21 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import { useSession } from "next-auth/react"
+import { useEffect, useState } from 'react'
 
 export default function TimeSheet() {
   const { data: session } = useSession()
-  const userId = session.user.id;
+  const userId = session?.user?.id;
 
-  fetch(`api/entries/${userId}`, { 
-    method: 'GET',
-  })
-    .then(response => response.json())
-    .then(allEntries => console.log(allEntries));
+  const [allEntries, setAllEntries] = useState([])
+
+  useEffect(() => {
+    fetch(`api/entries/${userId}`, { 
+      method: 'GET',
+    })
+      .then(response => response.json())
+      .then(allEntries => setAllEntries(allEntries.allUsersEntries));
+  }, [])
   
   return (
     <>
@@ -19,7 +24,14 @@ export default function TimeSheet() {
       <h2>Timesheet</h2>
         This is Timesheet....
         <ul>
-
+          {
+            allEntries.map(entry => (
+              <li key={entry._id}>
+                START: {entry.startTime}<br></br>
+                END: {entry.endTime}<br></br><br></br>
+              </li>
+            ))
+          }
         </ul>
       </section>
       <Footer />
