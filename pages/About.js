75664888f3737640687d33cd3ca691437497678d
salt-dev <salt-dev@Admins-MacBook-Pro-4.local>
2022-11-25 13:13:22 +0100

    about page doneish

--- pages/About.js
+++ pages/About.js
@@ -9,12 +9,28 @@ const About = () => {
     <Header />
     <section className="about-container">
       <h2>About Us</h2>
-      <p className="about">This app was created by the PepperShakers from &lt;/salt&gt; School of applied technology.
+      <p className="about">This app was created by the PepperShakers from &lt;/salt&gt; School of applied technology Stockholm.
         The team is made up of Pawel Obrzut, Nisha Chavan, Manoj Singh, and Claudia Carion.
         For three months, we went through the &lt;/salt&gt; Fullstack Javascript bootcamp.
         This is our final project, and the idea for the app was inspired by our daily attendance marking during the bootcamp.</p>
     </section>
-    <section></section>section classn
+    <section className="contactinfo">
+    <h3>Github</h3>
+    <ul className="github-container">
+      <li><a href="https://github.com/PawelObrzut">Pawel Obrzut</a></li>
+      <li><a href="https://github.com/nisha8c">Nisha Chavan</a></li>
+      <li><a href="https://github.com/MartianCoder79">Manoj Singh</a></li>
+      <li><a href="https://github.com/claudiacarion">Claudia Carion</a></li>
+    </ul>
+    <h3>LinkedIn</h3>
+    <ul className="linkedin-container">
+      <li><a href="https://www.linkedin.com/in/pawel-obrzut-023762110/">Pawel Obrzut</a></li>
+      <li><a href="https://www.linkedin.com/in/nisha-c-a15b59220/">Nisha Chavan</a></li>
+      <li><a href="https://www.linkedin.com/in/manoj-singh-4772b8255/">Manoj Singh</a></li>
+      <li><a href="https://www.linkedin.com/in/claudia-pereira-carion/">Claudia Carion</a></li>
+    </ul>
+
+    </section>
     <Footer />
     </>
   )
