5a346ca317c6fbe7abf5ec24f9cfcaa751ebe46f
salt-dev <salt-dev@Admins-MacBook-Pro-4.local>
2022-11-29 08:34:59 +0100

    pfp and css changed for better display

--- pages/Profile.js
+++ pages/Profile.js
@@ -13,7 +13,7 @@ const Profile = () => {
       <Header />
       <div className="profile-container">
         <h2>Your Profile</h2>
-        {/* <div className="profileInfo">{<picture><img className="profilePic" alt="profile pic" src={session?.user?.image}></img></picture>}</div> */}
+        <div className="profileInfo">{<picture><img className="profilePic" alt="profile pic" src={session?.user?.image}></img></picture>}</div>
         <div className="profileInfo">Name: {session?.user.name}</div>
         <div className="profileInfo">Email: {session?.user.email}</div>
       </div>
