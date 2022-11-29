635541eff45f881e643c93164212b04c6dc028bd
salt-dev <salt-dev@Admins-MacBook-Pro-4.local>
2022-11-29 08:30:06 +0100

    fixed pfp and some css for better display

--- pages/Profile.js
+++ pages/Profile.js
@@ -3,7 +3,6 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Link from 'next/link';
 import { useSession } from "next-auth/react";
-import Image from 'next/image';
 
 const Profile = () => {
 
@@ -14,12 +13,10 @@ const Profile = () => {
       <Header />
       <div className="profile-container">
         <h2>Your Profile</h2>
-        {/* <div className="profileInfo">{<img className="profilePic" src={session.user.image}></img>}</div> */}
+        <div className="profileInfo">{<picture><img className="profilePic" alt="profile pic" src={session?.user?.image}></img></picture>}</div>
         <div className="profileInfo">Name: {session?.user.name}</div>
         <div className="profileInfo">Email: {session?.user.email}</div>
-        
       </div>
-      {/* <Link href="/Settings">Go Back To Settings</Link> */}
       <Footer />
     </>
   )
